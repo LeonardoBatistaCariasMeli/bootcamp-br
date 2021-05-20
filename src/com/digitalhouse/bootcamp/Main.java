023fdbd3b1146e16b2d5ffd4d292dc9e0166a45e
Onias da Rocha Filho <onias.filho@gmail.com>
2021-05-20 10:44:31 -0400

    resolve exercicio 3

--- src/com/digitalhouse/bootcamp/Main.java
+++ src/com/digitalhouse/bootcamp/Main.java
@@ -1,7 +1,9 @@
 package com.digitalhouse.bootcamp;
 
+import com.digitalhouse.bootcamp.aula3.Aula3;
+
 public class Main {
     public static void main(String[] args) {
-
+        System.out.println(Aula3.isPrimo(5));
     }
 }
--- src/com/digitalhouse/bootcamp/aula3/Aula3.java
+++ src/com/digitalhouse/bootcamp/aula3/Aula3.java
@@ -1,4 +1,29 @@
 package com.digitalhouse.bootcamp.aula3;
 
 public class Aula3 {
+
+
+    public static Boolean isPrimo(int numero){
+
+        //    Exercicio 3
+        //Desenvolva um programa para informar se um um número n é primo ou não, sendo n um valor que o
+        // usuário irá inserir pelo console. Lembre-se que um número é primo quando só é divisível por 1 e por si mesmo.
+
+
+        boolean isPrimo = false;
+        int resultado = 0;
+
+        for(int i = 2; i<=numero / 2; i++){
+            if(numero % i == 0){
+                resultado++;
+                break;
+            }
+        }
+
+        if(resultado == 0){
+            isPrimo = true;
+        }
+
+        return isPrimo;
+    }
 }
