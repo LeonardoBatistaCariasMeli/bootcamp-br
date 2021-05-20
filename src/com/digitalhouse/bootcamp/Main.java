f60fdb1747c3ee0542dc2d00ffd8e612d07ff020
Nycolas De Lima Vieira <nyvieira@mbpdemeliadmin.lan>
2021-05-20 11:53:39 -0300

    java aula 3 exercicio 1.2

--- src/com/digitalhouse/bootcamp/Main.java
+++ src/com/digitalhouse/bootcamp/Main.java
@@ -1,7 +1,15 @@
 package com.digitalhouse.bootcamp;
 
+import com.digitalhouse.bootcamp.aula3.Aula3;
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
+        Scanner scanner = new Scanner(System.in);
+
+        Aula3 aula3 = new Aula3(scanner);
+        aula3.Exercicio2();
     }
 }
--- src/com/digitalhouse/bootcamp/aula3/Aula3.java
+++ src/com/digitalhouse/bootcamp/aula3/Aula3.java
@@ -1,4 +1,34 @@
 package com.digitalhouse.bootcamp.aula3;
 
+import java.util.Scanner;
+
 public class Aula3 {
+
+    private Scanner scanner;
+
+    public Aula3(Scanner scanner) {
+        this.scanner = scanner;
+    }
+
+    public void Exercicio2() {
+        System.out.print("Insira um número múltiplo: ");
+        int m = scanner.nextInt();
+        System.out.print("Insira uma quantidade de números: ");
+        int n = scanner.nextInt();
+
+        System.out.println("");
+
+        int i = 1;
+        do
+        {
+            if (i % m == 0) {
+                System.out.print(i);
+                System.out.print("\t");
+                n--;
+            }
+
+            i++;
+        } while (n > 0);
+    }
+
 }
