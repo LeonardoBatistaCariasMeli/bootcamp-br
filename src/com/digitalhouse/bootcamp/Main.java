ff0b1862e2e7dbef6b43e462443a7d969949b67e
LeonardoBatistaCariasMeli <leonardo.carias@mercadolivre.com>
2021-05-20 15:53:31 -0300

    Criacao do metodo runAllExercises

--- src/com/digitalhouse/bootcamp/Main.java
+++ src/com/digitalhouse/bootcamp/Main.java
@@ -13,31 +13,5 @@ public class Main {
 
         aula3.orderByAscAndDesc();
 
-//        // Exercicio 1
-//        System.out.println("Exercício 1");
-//        Aula3.exercicio1();
-//        System.out.println("\n");
-//
-//        // Exercicio 2
-//        System.out.println("Exercício 2");
-//        aula3.multipleNumbers();
-//        System.out.println("\n");
-//
-//        // Exercicio 3
-//        System.out.println("Exercício 3");
-//        System.out.print("Insira um número: ");
-//        int numero = scanner.nextInt();
-//        System.out.println(Aula3.isPrimo(numero) + "\n");
-//
-//        // Exercicio 4
-//        System.out.println("Exercício 4");
-//        aula3.primeNumbers();
-//        System.out.println("\n");
-//
-//        // Exercicio 5
-//        System.out.println("Exercício 5");
-//        aula3.firstPrimeNumberValuesWithConditions();
-//        System.out.println("");
-
     }
 }
