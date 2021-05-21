93bffd582221dc0ddc5c3581c518c15c5e71fca4
LeonardoBatistaCariasMeli <leonardo.carias@mercadolivre.com>
2021-05-21 11:30:31 -0300

    Ajustes

--- src/com/digitalhouse/bootcamp/Main.java
+++ src/com/digitalhouse/bootcamp/Main.java
@@ -1,18 +1,18 @@
 package com.digitalhouse.bootcamp;
 
-import com.digitalhouse.bootcamp.aula1.Aula1;
-
 import java.util.Scanner;
+
+import com.digitalhouse.bootcamp.aula1.Aula1;
 
 public class Main {
 
-    private static final Scanner scanner = new Scanner(System.in);
+	private static final Scanner scanner = new Scanner(System.in);
 
-    public static void main(String[] args) {
-        Aula1 aula1 = new Aula1(scanner);
+	public static void main(String[] args) {
+		Aula1 aula1 = new Aula1(scanner);
 
-        //aula1.runAllExercises();
-        aula1.readProductsAndPrint();
+		// aula1.runAllExercises();
+		aula1.readProductsAndPrint();
 
-    }
+	}
 }
--- src/com/digitalhouse/bootcamp/aula1/radixSort/RadixSort.java
+++ src/com/digitalhouse/bootcamp/aula1/radixSort/RadixSort.java
@@ -6,107 +6,110 @@ import java.util.Map;
 
 public class RadixSort {
 
-    public static void main(String[] args) {
-        RadixSort radix = new RadixSort();
+	private final int INITIAL_ARRAY[] = { 16223, 898, 13, 906, 235, 23, 9, 1532, 6388, 2511, 8 };
 
-        radix.radixSort();
-    }
+	public static void main(String[] args) {
+		RadixSort radix = new RadixSort();
 
-    public void radixSort() {
-        int iArr[] = {16223, 898, 13, 906, 235, 23, 9, 1532, 6388, 2511, 8};
+		radix.radixSort();
+	}
 
-        String str = Arrays.toString(iArr).replaceAll("\\s+", "");
+	public void radixSort() {
 
-        String[] iArrStr = str.substring(1, str.length() - 1).split(",");
+		String str = Arrays.toString(INITIAL_ARRAY).replaceAll("\\s+", "");
 
-        int maxLength = findMaxLength(iArrStr);
+		String[] iArrStr = str.substring(1, str.length() - 1).split(",");
 
-        String[] finalArr = sort(iArrStr, maxLength, 1);
+		int maxLength = findMaxLength(iArrStr);
 
-        for (String elem : finalArr) {
-            System.out.print(elem);
-            System.out.print("\t");
-        }
-    }
+		String[] finalArr = sort(iArrStr, maxLength, 1);
 
-    private String[] sort(String[] iArrStr, int maxLength, int actualPosition) {
-        if (actualPosition > maxLength) {
-            return iArrStr;
-        }
+		for (String elem : finalArr) {
+			System.out.print(elem);
+			System.out.print("\t");
+		}
+	}
 
-        HashMap<Integer, Integer> values = getNewHashMap();
-        fillHashMap(values, iArrStr, actualPosition);
+	private String[] sort(String[] iArrStr, int maxLength, int actualPosition) {
+		if (actualPosition > maxLength) {
+			return iArrStr;
+		}
 
-        sumHashMapWithNextPosition(values);
+		HashMap<Integer, Integer> values = getNewHashMap();
+		fillHashMap(values, iArrStr, actualPosition);
 
-        String[] newArrStr = createNewPositionArray(values, iArrStr, actualPosition);
+		sumHashMapWithNextPosition(values);
 
-        return sort(newArrStr, maxLength, ++actualPosition);
-    }
+		String[] newArrStr = createNewPositionArray(values, iArrStr, actualPosition);
 
-    public int findMaxLength(String[] iArrStr) {
-        int max = 0;
-        for (String elem : iArrStr) {
-            int auxMax = elem.length();
+		return sort(newArrStr, maxLength, ++actualPosition);
+	}
 
-            if (max < auxMax)
-                max = auxMax;
-        }
-        return max;
-    }
+	public int findMaxLength(String[] iArrStr) {
+		int max = 0;
+		for (String elem : iArrStr) {
+			int auxMax = elem.length();
 
-    private HashMap<Integer, Integer> getNewHashMap() {
-        HashMap<Integer, Integer> values = new HashMap<>();
-        values.put(0, 0);
-        values.put(1, 0);
-        values.put(2, 0);
-        values.put(3, 0);
-        values.put(4, 0);
-        values.put(5, 0);
-        values.put(6, 0);
-        values.put(7, 0);
-        values.put(8, 0);
-        values.put(9, 0);
-        return values;
-    }
+			if (max < auxMax)
+				max = auxMax;
+		}
+		return max;
+	}
 
-    public void fillHashMap(HashMap<Integer, Integer> values, String[] iArrStr, int actualPosition) {
-        for (String elem : iArrStr) {
-            int digit = 0;
-            if (!(elem.length() < actualPosition)) {
-                digit = Integer.parseInt(elem.substring(elem.length() - actualPosition, elem.length() - actualPosition + 1));
-            }
-            int value = values.get(digit);
-            values.put(digit, ++value);
-        }
-    }
+	private HashMap<Integer, Integer> getNewHashMap() {
+		HashMap<Integer, Integer> values = new HashMap<>();
+		values.put(0, 0);
+		values.put(1, 0);
+		values.put(2, 0);
+		values.put(3, 0);
+		values.put(4, 0);
+		values.put(5, 0);
+		values.put(6, 0);
+		values.put(7, 0);
+		values.put(8, 0);
+		values.put(9, 0);
+		return values;
+	}
 
-    public void sumHashMapWithNextPosition(HashMap<Integer, Integer> values) {
-        for (Map.Entry<Integer, Integer> entry : values.entrySet()) {
-            int key = entry.getKey();
-            if (!(key == 0)) {
-                int value = entry.getValue();
-                int auxKey = entry.getKey() - 1;
-                int auxValue = values.get(auxKey);
-                values.put(key, auxValue + value);
-            }
-        }
-    }
+	public void fillHashMap(HashMap<Integer, Integer> values, String[] iArrStr, int actualPosition) {
+		for (String elem : iArrStr) {
+			int digit = 0;
+			if (!(elem.length() < actualPosition)) {
+				digit = Integer
+						.parseInt(elem.substring(elem.length() - actualPosition, elem.length() - actualPosition + 1));
+			}
+			int value = values.get(digit);
+			values.put(digit, ++value);
+		}
+	}
 
-    public String[] createNewPositionArray(HashMap<Integer,Integer> values, String[] iArrStr, int actualPosition) {
-        String[] newArrStr = new String[iArrStr.length];
-         for(int i = iArrStr.length - 1; i > -1; i --) {
-            String elem = iArrStr[i];
-            int digit = 0;
+	public void sumHashMapWithNextPosition(HashMap<Integer, Integer> values) {
+		for (Map.Entry<Integer, Integer> entry : values.entrySet()) {
+			int key = entry.getKey();
+			if (!(key == 0)) {
+				int value = entry.getValue();
+				int auxKey = entry.getKey() - 1;
+				int auxValue = values.get(auxKey);
+				values.put(key, auxValue + value);
+			}
+		}
+	}
 
-            if(!(elem.length() < actualPosition)) {
-                digit = Integer.parseInt(elem.substring(elem.length()-actualPosition, elem.length()-actualPosition +1));
-            }
+	public String[] createNewPositionArray(HashMap<Integer, Integer> values, String[] iArrStr, int actualPosition) {
+		String[] newArrStr = new String[iArrStr.length];
+		for (int i = iArrStr.length - 1; i > -1; i--) {
+			String elem = iArrStr[i];
+			int digit = 0;
 
-            int value = values.get(digit);
-            values.put(digit, --value);
-            newArrStr[value] = iArrStr[i];
-        }
-         return newArrStr;
-    }
+			if (!(elem.length() < actualPosition)) {
+				digit = Integer
+						.parseInt(elem.substring(elem.length() - actualPosition, elem.length() - actualPosition + 1));
+			}
+
+			int value = values.get(digit);
+			values.put(digit, --value);
+			newArrStr[value] = iArrStr[i];
+		}
+		return newArrStr;
+	}
 }
