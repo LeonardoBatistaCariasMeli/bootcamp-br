1441a4450899e007a2392a5d0636b72a0d36c275
Gustavo <gustavo.franca@mercadolivre.com>
2021-05-21 14:49:31 -0300

    feat: adiciona exercicio 5

---
+++ src/com/digitalhouse/bootcamp/aula2/exercicio5/Teste.java
@@ -0,0 +1,9 @@
+package com.digitalhouse.bootcamp.aula2.exercicio5;
+
+public class Teste {
+
+    public static void main(String[] args) {
+        System.out.println( new Date(2,11,2021));
+
+    }
+}
